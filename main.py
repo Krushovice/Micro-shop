6ae05e9431e6187b52371c1c03bbdf7a5d614f88
Krushovice <Kickstar69@yandex.ru>
2024-01-22 08:01:54 +0000

    No more create all in main

--- main.py
+++ main.py
@@ -9,15 +9,11 @@ from fastapi.templating import Jinja2Templates
 from api_v1 import router as router_v1
 from items.views import router as items_router
 from users.views import router as users_router
-from core.models import Base, db_helper
 from core.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
 
 
