1b6a28d904033bb0c075f4a8e06af6e5ac53b822
Krushovice <Kickstar69@yandex.ru>
2024-01-22 09:38:07 +0000

    Создана миграция с таблицей users

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,12 @@
 __all__ = {
     "Base",
     "Product",
+    "User",
     "DataBaseHelper",
     "db_helper",
 }
 
 from .base import Base  # noqa
 from .product import Product  # noqa
+from .user import User  # noqa
 from .db_helper import DataBaseHelper, db_helper  # noqa
