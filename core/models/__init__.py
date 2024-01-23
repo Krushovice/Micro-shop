1f4f4b12fb7c4670e1200774df7cc8304eee7aeb
Krushovice <Kickstar69@yandex.ru>
2024-01-23 05:47:16 +0000

    Создана модель Post

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,6 +2,7 @@ __all__ = {
     "Base",
     "Product",
     "User",
+    "Post",
     "DataBaseHelper",
     "db_helper",
 }
@@ -9,4 +10,5 @@ __all__ = {
 from .base import Base  # noqa
 from .product import Product  # noqa
 from .user import User  # noqa
+from .post import Post  # noqa
 from .db_helper import DataBaseHelper, db_helper  # noqa
